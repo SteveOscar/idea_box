12609d599810bbf38ee8d45085d554725cb433c0
Steven Olson <steveoscaro@gmail.com>
2016-03-21 17:21:33 -0600

    slight refactor

--- app/assets/javascripts/create.js
+++ app/assets/javascripts/create.js
@@ -6,21 +6,24 @@ function createIdea() {
         body: $("#idea-body").val()
       }
     }
-    $.ajax({
-      type: 'POST',
-      url : '/api/v1/ideas.json',
-      data: ideaParams,
-      success: function(newIdea) {
-        renderIdea(newIdea)
-      },
-      error: function(xhr) {
-        console.log(xhr.responseText)
-      }
-    })
+    saveToDatabase(ideaParams);
   })
   clearInputs();
 }
 
+function saveToDatabase(ideaParams) {
+  $.ajax({
+    type: 'POST',
+    url : '/api/v1/ideas.json',
+    data: ideaParams,
+    success: function(newIdea) {
+      renderIdea(newIdea)
+    },
+    error: function(xhr) {
+      console.log(xhr.responseText)
+    }
+  })
+}
 function clearInputs() {
   $('input#idea-title').val('New Title');
   $('input#idea-body').val('New Idea');
