c3c0f443cf812b86a351b95e3172d14b2f8dab40
Steven Olson <steveoscaro@gmail.com>
2016-03-23 14:10:20 -0600

    add model testing for the Idea model

--- test/models/idea_test.rb
+++ test/models/idea_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class IdeaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "idea creation" do
+    start = Idea.count
+    Idea.create(title: 'new idea', body: 'new body')
+    finish = Idea.count
+
+    refute start == finish
+    assert_equal Idea.last.title, 'new idea'
+  end
+
+  test "idea creation with missing title" do
+    start = Idea.count
+    Idea.create(body: 'new body')
+    finish = Idea.count
+
+    assert start == finish
+    refute_equal Idea.last.body, 'new idea'
+  end
+
+  test "idea creation with missing body" do
+    start = Idea.count
+    Idea.create(title: 'new title')
+    finish = Idea.count
+
+    assert start == finish
+    refute_equal Idea.last.title, 'new title'
+  end
 end
